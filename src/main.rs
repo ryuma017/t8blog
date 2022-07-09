25b8b581e931a5ea032dd3bcab8c9482e8a29698
ryuma017 <tgcrym01@gmail.com>
2022-07-09 23:20:42 +0900

    build startup logic

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
-fn main() {
-    println!("Hello, world!");
+use t8blog::{configuration::get_configuration, startup::Application};
+
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+    let configuration = get_configuration().expect("Failed to read configuration");
+    let application = Application::build(configuration).await?;
+    application.run_server().await?;
+    Ok(())
 }
--- src/startup.rs
+++ src/startup.rs
@@ -1,1 +1,38 @@
+use std::net::TcpListener;
 
+use actix_web::{dev::Server, web, App, HttpServer};
+
+use crate::configuration::Settings;
+use crate::routes::health_check;
+
+pub struct Application {
+    port: u16,
+    server: Server,
+}
+
+impl Application {
+    pub async fn build(configuration: Settings) -> Result<Self, std::io::Error> {
+        let address = format!(
+            "{}:{}",
+            configuration.application.host, configuration.application.port
+        );
+        let listener = TcpListener::bind(address)?;
+        let port = listener.local_addr().unwrap().port();
+        let server = build_server(listener)?;
+
+        Ok(Self { port, server })
+    }
+
+    pub async fn run_server(self) -> Result<(), std::io::Error> {
+        self.server.await
+    }
+}
+
+fn build_server(listener: TcpListener) -> Result<Server, std::io::Error> {
+    let server =
+        HttpServer::new(move || App::new().route("/health_check", web::get().to(health_check)))
+            .listen(listener)?
+            .run();
+
+    Ok(server)
+}
