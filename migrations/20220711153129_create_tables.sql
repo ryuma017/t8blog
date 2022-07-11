0d27616004a4855d52eee6ef2fcf744a6a108b49
ryuma017 <tgcrym01@gmail.com>
2022-07-12 03:05:24 +0900

    fix sql syntax

--- migrations/20220711153129_create_tables.sql
+++ migrations/20220711153129_create_tables.sql
@@ -1,12 +1,12 @@
-CREATE TABLE users {
+CREATE TABLE users (
     id               UUID NOT NULL,
     name             TEXT NOT NULL,
     create_timestamp TIMESTAMP NOT NULL,
     update_timestamp TIMESTAMP,
     PRIMARY KEY (id)
-}
+);
 
-CREATE TABLE articles {
+CREATE TABLE articles (
     id               UUID NOT NULL,
     author_id        UUID NOT NULL,
     title            TEXT NOT NULL,
@@ -16,15 +16,15 @@ CREATE TABLE articles {
     update_timestamp TIMESTAMP NOT NULL,
     PRIMARY KEY (id),
     FOREIGN KEY (author_id) REFERENCES users (id) ON DELETE CASCADE
-}
+);
 
-CREATE TABLE likes {
-    id UUID NOT NULL,
-    article_id UUID NOT NULL,
-    user_id UUID NOT NULL,
-    create_timestamp TIMESTAMP DEFAULT '0000-00-00',
+CREATE TABLE likes (
+    id               UUID NOT NULL,
+    article_id       UUID NOT NULL,
+    user_id          UUID NOT NULL,
+    create_timestamp TIMESTAMP DEFAULT '1000-01-01 00:00:00',
+    UNIQUE (user_id, article_id),
     PRIMARY KEY (id),
-    UNIQUE KEY (user_id, article_id),
     FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE,
     FOREIGN KEY (article_id) REFERENCES articles (id) ON DELETE CASCADE
-}
+);
