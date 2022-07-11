01b50329cb2be0eeda27a995aad413a40fac8271
ryuma017 <tgcrym01@gmail.com>
2022-07-12 01:31:19 +0900

    create a migration to create tables

---
+++ migrations/20220711153129_create_tables.sql
@@ -0,0 +1,30 @@
+CREATE TABLE users {
+    id               UUID NOT NULL,
+    name             TEXT NOT NULL,
+    create_timestamp TIMESTAMP NOT NULL,
+    update_timestamp TIMESTAMP,
+    PRIMARY KEY (id)
+}
+
+CREATE TABLE articles {
+    id               UUID NOT NULL,
+    author_id        UUID NOT NULL,
+    title            TEXT NOT NULL,
+    content          TEXT NOT NULL,
+    like_count       INTEGER NOT NULL DEFAULT 0,
+    create_timestamp TIMESTAMP NOT NULL,
+    update_timestamp TIMESTAMP NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (author_id) REFERENCES users (id) ON DELETE CASCADE
+}
+
+CREATE TABLE likes {
+    id UUID NOT NULL,
+    article_id UUID NOT NULL,
+    user_id UUID NOT NULL,
+    create_timestamp TIMESTAMP DEFAULT '0000-00-00',
+    PRIMARY KEY (id),
+    UNIQUE KEY (user_id, article_id),
+    FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE,
+    FOREIGN KEY (article_id) REFERENCES articles (id) ON DELETE CASCADE
+}
